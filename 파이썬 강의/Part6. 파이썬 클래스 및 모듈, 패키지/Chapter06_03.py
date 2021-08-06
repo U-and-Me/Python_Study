2db0227b60d312abb8d82b9ee15f1f6583972621
Kim Yu Na <81612540+U-and-Me@users.noreply.github.com>
2021-08-06 15:34:17 +0900

    Create Chapter06_03.py

---
+++ 파이썬 강의/Part6. 파이썬 클래스 및 모듈, 패키지/Chapter06_03.py
@@ -0,0 +1,44 @@
+#Chapter06-3
+# 파이썬 패키지 작성 및 사용법
+# 패키지로 분할된 개별적인 모듈로 구성
+# 상대경로 : ..(부모 디렉토리), .(현재 디렉토리)
+
+# 예제1
+import sub.sub1.module1
+import sub.sub2.module2
+
+# 사용
+sub.sub1.module1.mod1_test1()
+sub.sub1.module1.mod1_test2()
+
+sub.sub2.module2.mod2_test1()
+sub.sub2.module2.mod2_test2()
+
+print()
+print()
+print()
+
+# 예제2
+from sub.sub1 import module1
+from sub.sub2 import module2 as m2 # alias
+
+module1.mod1_test1()
+module1.mod1_test2()
+
+m2.mod2_test1()
+m2.mod2_test2()
+
+print()
+print()
+print()
+
+# 예제3
+from sub.sub1 import *
+from sub.sub2 import *
+
+module1.mod1_test1()
+module1.mod1_test2()
+
+module2.mod2_test1()
+module2.mod2_test2()
+
